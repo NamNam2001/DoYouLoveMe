d65a524d76d20e2c0478167e1235244bf2285059
rice <tonthatanhminhktpm@gmail.com>
2022-04-02 21:39:27 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const btnNo = document.getElementById("no");
+const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
+btnNo.addEventListener("mouseover",function(){
+    btnNo.style.left= getRandom(0, 500)+'px'; // 👈🏼 Horizontally
+    btnNo.style.top = getRandom(0, 500)+'px'; // 👈🏼 Vertically
+
+})
+
+
+
+function yes(){
+    alert("Tớ biết mà <3 !!!");
+}
